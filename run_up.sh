c1b2cef269c5a4d0c07336797b4f418f6e461fc8
Jxu-Thu <jxu3425@gmail.com>
2021-03-28 21:21:16 +0800

    data_dir

---
+++ run_up.sh
@@ -0,0 +1,16 @@
+python scripts/preprocess/build_vocabulary.py \
+    --captions /blob/v-jinx/data/COCO/annotations/captions_train2017.json \
+    --vocab-size 10000 \
+    --output-prefix /blob/v-jinx/data/COCO/virtex/vocab/coco_10k \
+    --do-lower-case
+
+
+python scripts/preprocess/preprocess_coco.py \
+    --data-root /blob/v-jinx/data/COCO \
+    --split train \
+    --output /blob/v-jinx/data/COCO/virtex/serialized_train.lmdb
+
+python scripts/preprocess/preprocess_coco.py \
+    --data-root /blob/v-jinx/data/COCO/ \
+    --split val \
+    --output /blob/v-jinx/data/COCO/virtex/serialized_val.lmdb
