18f3b587d8a5b564168b8ceb7020e1a9effd0637
Jxu-Thu <jxu3425@gmail.com>
2021-03-29 23:32:19 +0800

    data_dir

--- submit_run1.sh
+++ submit_run1.sh
@@ -34,16 +34,16 @@ sudo chmod 0440 /etc/sudoers.d/${PHILLY_USER}
 
 
 cd /blob/v-jinx/virtex
+pip install tensorflow==2.4.1 --user
 pip install -r requirements.txt &>/dev/null
 python setup.py develop --user
-pip install tensorboard==1.15.0 --user
 
 
 python scripts/pretrain_virtex.py \
     --config configs/_base_bicaptioning_R_50_L1_H1024.yaml \
     --num-gpus-per-machine 8 \
     --cpu-workers 4 \
-    --serialization-dir /tmp/VIRTEX_R_50_L1_H1024
+    --serialization-dir /blob/v-jinx/checkpoint_virtex/VIRTEX_R_50_L1_H1024
 
 #python scripts/pretrain_virtex.py \
 #    --config configs/_base_bicaptioning_R_50_L1_H1024.yaml \
