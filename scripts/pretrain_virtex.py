a755037ebd2a2ddbba06a05c4066eddad6d41d51
Jxu-Thu <jxu3425@gmail.com>
2021-03-31 20:57:40 +0800

    data_dir

--- scripts/pretrain_virtex.py
+++ scripts/pretrain_virtex.py
@@ -201,8 +201,6 @@ def main(_A: argparse.Namespace):
         # ---------------------------------------------------------------------
         #   VALIDATION
         # ---------------------------------------------------------------------
-        import pdb
-        pdb.set_trace()
         if iteration % _A.checkpoint_every == 0:
             if dist.is_master_process():
                 checkpoint_manager.step(iteration)
