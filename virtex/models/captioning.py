93f187ee14f62c16e714e3f4f2a81e7d3d35a924
Jxu-Thu <jxu3425@gmail.com>
2021-03-31 20:30:19 +0800

    data_dir

--- virtex/models/captioning.py
+++ virtex/models/captioning.py
@@ -117,7 +117,9 @@ class CaptioningModel(nn.Module):
         # shape: (batch_size, channels, height, width)
         visual_features = self.visual(batch["image"])
         batch_size = visual_features.size(0)
-
+        
+        import pdb
+        pdb.set_trace()
         if "caption_tokens" in batch:
             caption_tokens = batch["caption_tokens"]
             caption_lengths = batch["caption_lengths"]
