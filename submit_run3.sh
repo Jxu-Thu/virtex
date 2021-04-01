f54cbde4050eb92f2ff338a6fecc7ce05e57e32e
Jxu-Thu <jxu3425@gmail.com>
2021-04-01 12:19:41 +0800

    data_dir

--- submit_run3.sh
+++ submit_run3.sh
@@ -53,3 +53,14 @@ pip install tensorflow==2.4.1 --user
 pip install -r requirements.txt --user
 python setup.py develop --user
 python -m pip install 'git+https://github.com/facebookresearch/detectron2.git' --user
+
+export DETECTRON2_DATASETS=/blob/v-jinx/data
+CUDA_VISIBLE_DEVICES=0 python scripts/eval_detectron2.py \
+    --config /blob/v-jinx/checkpoint_virtex/VIRTEX_R_50_L1_H1024/pretrain_config.yaml \
+    --d2-config configs/detectron2/coco_segm_default_init_2x.yaml \
+    --checkpoint-path /blob/v-jinx/checkpoint_virtex/VIRTEX_R_50_L1_H1024/checkpoint_500000.pth \
+    --weight-init virtex \
+    --num-gpus-per-machine 8 \
+    --cpu-workers 2 \
+    --serialization-dir /blob/v-jinx/checkpoint_virtex/VIRTEX_R_50_L1_H1024/coco_segm_500000 \
+    --checkpoint-every 5000
