ce83691c657a5dc8044b45ba37d484221bd56865
Jxu-Thu <jxu3425@gmail.com>
2021-04-01 11:32:55 +0800

    data_dir

--- debug.sh
+++ debug.sh
@@ -13,7 +13,18 @@
 #    --do-lower-case
 
 
-python scripts/preprocess/preprocess_coco.py \
-    --data-root /blob/v-jinx/data/COCO/ \
-    --split val \
-    --output /blob/v-jinx/data/COCO/virtex/serialized_val.lmdb.debug
+#python scripts/preprocess/preprocess_coco.py \
+#    --data-root /blob/v-jinx/data/COCO/ \
+#    --split val \
+#    --output /blob/v-jinx/data/COCO/virtex/serialized_val.lmdb.debug
+
+
+CUDA_VISIBLE_DEVICES=0 python scripts/eval_detectron2.py \
+    --config /blob/v-jinx/checkpoint_virtex/VIRTEX_R_50_L1_H1024/pretrain_config.yaml \
+    --d2-config configs/detectron2/coco_segm_default_init_2x.yaml \
+    --checkpoint-path /blob/v-jinx/checkpoint_virtex/VIRTEX_R_50_L1_H1024/checkpoint_500000.pth \
+    --weight-init virtex \
+    --num-gpus-per-machine 1 \
+    --cpu-workers 2 \
+    --serialization-dir /blob/v-jinx/checkpoint_virtex/VIRTEX_R_50_L1_H1024/coco_segm_500000 \
+    --checkpoint-every 5000
