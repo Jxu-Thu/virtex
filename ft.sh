215cd0c7cdc04f246528c0165972b43575e35c27
Jxu-Thu <jxu3425@gmail.com>
2021-04-01 11:14:29 +0800

    data_dir

---
+++ ft.sh
@@ -0,0 +1,10 @@
+# CoCo detection /
+python scripts/eval_detectron2.py \
+    --config /blob/v-jinx/checkpoint_virtex/VIRTEX_R_50_L1_H1024/pretrain_config.yaml \
+    --d2-config configs/detectron2/coco_segm_default_init_2x.yaml \
+    --checkpoint-path /blob/v-jinx/checkpoint_virtex/VIRTEX_R_50_L1_H1024/checkpoint_500000.pth \
+    --weight-init virtex \
+    --num-gpus-per-machine 8 \
+    --cpu-workers 2 \
+    --serialization-dir /tmp/bicaptioning_R_50_L1_H2048/coco_segm_500000 \
+    --checkpoint-every 5000
