0cae76c1da37a299ece8a7438a44e0a8ed452cef
Jxu-Thu <jxu3425@gmail.com>
2021-04-01 14:04:47 +0800

    data_dir

--- virtex/debug.sh
+++ virtex/debug.sh
@@ -19,22 +19,22 @@
 #    --output /blob/v-jinx/data/coco/virtex/serialized_val.lmdb.debug
 
 
-export DETECTRON2_DATASETS=/blob/v-jinx/data
-CUDA_VISIBLE_DEVICES=0 python scripts/eval_detectron2.py \
-    --config /blob/v-jinx/checkpoint_virtex/VIRTEX_R_50_L1_H1024/pretrain_config.yaml \
-    --d2-config configs/detectron2/coco_segm_default_init_2x.yaml \
-    --checkpoint-path /blob/v-jinx/checkpoint_virtex/VIRTEX_R_50_L1_H1024/checkpoint_500000.pth \
-    --weight-init virtex \
-    --num-gpus-per-machine 1 \
-    --cpu-workers 2 \
-    --serialization-dir /blob/v-jinx/checkpoint_virtex/VIRTEX_R_50_L1_H1024/coco_segm_500000 \
-    --checkpoint-every 5000
-
-
-
-#CUDA_VISIBLE_DEVICES=0  python scripts/eval_captioning.py \
+#export DETECTRON2_DATASETS=/blob/v-jinx/data
+#CUDA_VISIBLE_DEVICES=0 python scripts/eval_detectron2.py \
 #    --config /blob/v-jinx/checkpoint_virtex/VIRTEX_R_50_L1_H1024/pretrain_config.yaml \
-#    --checkpoint-path  /blob/v-jinx/checkpoint_virtex/VIRTEX_R_50_L1_H1024/checkpoint_500000.pth \
-#    --calc-metrics \
+#    --d2-config configs/detectron2/coco_segm_default_init_2x.yaml \
+#    --checkpoint-path /blob/v-jinx/checkpoint_virtex/VIRTEX_R_50_L1_H1024/checkpoint_500000.pth \
+#    --weight-init virtex \
 #    --num-gpus-per-machine 1 \
-#    --cpu-workers 4
+#    --cpu-workers 2 \
+#    --serialization-dir /blob/v-jinx/checkpoint_virtex/VIRTEX_R_50_L1_H1024/coco_segm_500000 \
+#    --checkpoint-every 5000
+
+
+
+CUDA_VISIBLE_DEVICES=0  python scripts/eval_captioning.py \
+    --config /blob/v-jinx/checkpoint_virtex/VIRTEX_R_50_L1_H1024/pretrain_config.yaml \
+    --checkpoint-path  /blob/v-jinx/checkpoint_virtex/VIRTEX_R_50_L1_H1024/checkpoint_500000.pth \
+    --calc-metrics \
+    --num-gpus-per-machine 1 \
+    --cpu-workers 4
