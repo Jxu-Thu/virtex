f7e850f3ac30c2e6a5040b296e757f69895372f1
Jxu-Thu <jxu3425@gmail.com>
2021-04-05 16:45:25 +0800

    data_dir

--- UNITER/debug.sh
+++ UNITER/debug.sh
@@ -1,1 +1,4 @@
-python train_nlvr2.py --config config/train-nlvr2-base-1gpu.json --output_dir /blob/v-jinx/checkpoint_uniter/pre_train/finetune/nlvr2/debug --debug
+#python train_nlvr2.py --config config/train-nlvr2-base-1gpu.json --output_dir /blob/v-jinx/checkpoint_uniter/pre_train/finetune/nlvr2/debug --debug
+
+python pretrain_debug.py --config config/pretrain-indomain-base-8gpu.json \
+    --output_dir /blob/v-jinx/checkpoint_uniter/pre_train/pretrain/indomain_debug
