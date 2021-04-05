f383210c9470c2154aba39fc8e34c1ee9b821f88
Jxu-Thu <jxu3425@gmail.com>
2021-04-05 16:39:18 +0800

    data_dir

--- UNITER/submit_run1.sh
+++ UNITER/submit_run1.sh
@@ -2,5 +2,5 @@ cd /blob/v-jinx/virtex/UNITER
 python train_nlvr2.py --config config/train-nlvr2-base-1gpu.json --output_dir /blob/v-jinx/checkpoint_uniter/pre_train/finetune/nlvr2/submit
 
 
-#python inf_nlvr2.py --txt_db /blob/v-jinx/checkpoint_uniter/pre_train/txt_db/nlvr2_test1.db --img_db /blob/v-jinx/checkpoint_uniter/pre_train/img_db/nlvr2_test/ \
-#    --train_dir /blob/v-jinx/checkpoint_uniter/pre_train/finetune/nlvr2/submit --ckpt 6500 --output_dir /blob/v-jinx/checkpoint_uniter/pre_train/finetune/nlvr2/submit --fp16
+python inf_nlvr2.py --txt_db /blob/v-jinx/checkpoint_uniter/pre_train/txt_db/nlvr2_test1.db --img_db /blob/v-jinx/checkpoint_uniter/pre_train/img_db/nlvr2_test/ \
+    --train_dir /blob/v-jinx/checkpoint_uniter/pre_train/finetune/nlvr2/submit --ckpt 6500 --output_dir /blob/v-jinx/checkpoint_uniter/pre_train/finetune/nlvr2/submit --fp16
--- UNITER/submit_run2.sh
+++ UNITER/submit_run2.sh
@@ -1,3 +1,3 @@
 cd /blob/v-jinx/virtex/UNITER
 horovodrun -np 8 python pretrain.py --config config/pretrain-indomain-base-8gpu.json \
-    --output_dir --output_dir /blob/v-jinx/checkpoint_uniter/pre_train/pretrain/indomain_submit
+    --output_dir /blob/v-jinx/checkpoint_uniter/pre_train/pretrain/indomain_submit
