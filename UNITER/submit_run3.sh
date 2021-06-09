6af7c5f2750e84fcdd29855d32a7d80ee9642a3b
Jxu-Thu <jxu3425@gmail.com>
2021-06-09 19:28:40 +0800

    d

--- UNITER/submit_run3.sh
+++ UNITER/submit_run3.sh
@@ -1,2 +1,2 @@
 cd /blob/v-jinx/virtex/UNITER
-horovodrun -np 4 python train_vqa.py --config config/train-vqa-base-4gpu.json
+horovodrun -np 4 python train_vqa.py --config config/train-vqa-base-4gpu.json --output_dir /blob/v-jinx/checkpoint_uniter/pre_train/pretrain/vqa/default
