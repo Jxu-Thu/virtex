08de5bc9dc6ce378c1162e704f0e6e69b49fe237
Jxu-Thu <jxu3425@gmail.com>
2021-06-09 18:39:11 +0800

    d

--- UNITER/submit_run2.sh
+++ UNITER/submit_run2.sh
@@ -1,3 +1,2 @@
 cd /blob/v-jinx/virtex/UNITER
-horovodrun -np 8 python pretrain.py --config config/pretrain-indomain-base-8gpu.json \
-    --output_dir /blob/v-jinx/checkpoint_uniter/pre_train/pretrain/indomain_submit
+horovodrun -np 8 python pretrain.py --config config/pretrain-indomain-base-8gpu.json
