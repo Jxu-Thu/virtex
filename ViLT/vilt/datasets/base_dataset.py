eea9992ea2846b494a1602086a5ca394fa2b83c2
Jxu-Thu <jxu3425@gmail.com>
2021-06-13 13:18:25 +0800

    d

--- ViLT/vilt/datasets/base_dataset.py
+++ ViLT/vilt/datasets/base_dataset.py
@@ -40,6 +40,8 @@ class BaseDataset(torch.utils.data.Dataset):
         self.data_dir = data_dir
 
         if len(names) != 0:
+            import pdb
+            pdb.set_trace()
             tables = [
                 pa.ipc.RecordBatchFileReader(
                     pa.memory_map(f"{data_dir}/{name}.arrow", "r")
