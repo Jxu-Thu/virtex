2cb09669ddabab3a53a4e8e224af5e16351e2d80
Jxu-Thu <jxu3425@gmail.com>
2021-06-13 10:47:18 +0800

    d

--- ViLT/debug.sh
+++ ViLT/debug.sh
@@ -1,7 +1,33 @@
-export MASTER_ADDR=$DIST_0_IP
-export MASTER_PORT=$DIST_0_PORT
-export NODE_RANK=$DIST_RANK
+#MASTER_PORT=54321       # Port of master server
+#N_GPU_LOCAL=`nvidia-smi --query-gpu=name --format=csv,noheader | wc -l`
+#N_GPU_PER_WORKER=${DLTS_NUM_GPU_PER_WORKER:-${N_GPU_LOCAL}}
+#N_WORKER=${DLTS_NUM_WORKER:-1}
+#NODE_RANK=${DLTS_ROLE_IDX:-0}
+#MASTER_ADDR=${MASTER_IP:-127.0.0.1}
+#
+#export NCCL_TREE_THRESHOLD=0
+#export NCCL_ALGO=Ring
+#export NCCL_DEBUG=INFO
+#
+#
+#dist_config="-m torch.distributed.launch --nproc_per_node=${N_GPU_PER_WORKER} --nnodes=${N_WORKER} --node_rank=${NODE_RANK} \
+#    --master_addr="${MASTER_ADDR}" --master_port=${MASTER_PORT}"
+
+
+MASTER_PORT=54321       # Port of master server
+N_GPU_LOCAL=`nvidia-smi --query-gpu=name --format=csv,noheader | wc -l`
+N_GPU_PER_WORKER=${DLTS_NUM_GPU_PER_WORKER:-${N_GPU_LOCAL}}
+N_WORKER=${DLTS_NUM_WORKER:-1}
+NODE_RANK=${DLTS_ROLE_IDX:-0}
+MASTER_ADDR=${MASTER_IP:-127.0.0.1}
+export MASTER_ADDR=$MASTER_ADDR
+export MASTER_PORT=$MASTER_PORT
+export NODE_RANK=$NODE_RANK
+export NCCL_TREE_THRESHOLD=0
+export NCCL_ALGO=Ring
+export NCCL_DEBUG=INFO
+
 exp_name=debug_pretrain
 data_root=/blob/v-jinx/data/VilT_dataset
 log_dir=/blob/v-jinx/checkpoint_vilt/pre_train
-python run.py with data_root=${data_root} log_dir=${log_dir} exp_name=${exp_name} num_gpus=1 num_nodes=1 task_mlm_itm whole_word_masking=True step200k per_gpu_batchsize=32
+python run.py with data_root=${data_root} log_dir=${log_dir} exp_name=${exp_name} num_gpus=${N_GPU_PER_WORKER} num_nodes=${N_WORKER} task_mlm_itm whole_word_masking=True step200k per_gpu_batchsize=32
