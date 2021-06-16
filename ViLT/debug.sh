d6f74a412d6dc23322f25aeeff6749aa03401909
Jxu-Thu <jxu3425@gmail.com>
2021-06-16 18:48:49 +0800

    d

--- ViLT/debug.sh
+++ ViLT/debug.sh
@@ -14,18 +14,18 @@
 #    --master_addr="${MASTER_ADDR}" --master_port=${MASTER_PORT}"
 
 #CUDA_VISIBLE_DEVICES=0
-MASTER_PORT=54321       # Port of master server
-N_GPU_LOCAL=`nvidia-smi --query-gpu=name --format=csv,noheader | wc -l`
-N_GPU_PER_WORKER=${DLTS_NUM_GPU_PER_WORKER:-${N_GPU_LOCAL}}
-N_WORKER=${DLTS_NUM_WORKER:-1}
-NODE_RANK=${DLTS_ROLE_IDX:-0}
-MASTER_ADDR=${MASTER_IP:-127.0.0.1}
-export MASTER_ADDR=$MASTER_ADDR
-export MASTER_PORT=$MASTER_PORT
-export NODE_RANK=$NODE_RANK
-export NCCL_TREE_THRESHOLD=0
-export NCCL_ALGO=Ring
-export NCCL_DEBUG=INFO
+#MASTER_PORT=54321       # Port of master server
+#N_GPU_LOCAL=`nvidia-smi --query-gpu=name --format=csv,noheader | wc -l`
+#N_GPU_PER_WORKER=${DLTS_NUM_GPU_PER_WORKER:-${N_GPU_LOCAL}}
+#N_WORKER=${DLTS_NUM_WORKER:-1}
+#NODE_RANK=${DLTS_ROLE_IDX:-0}
+#MASTER_ADDR=${MASTER_IP:-127.0.0.1}
+#export MASTER_ADDR=$MASTER_ADDR
+#export MASTER_PORT=$MASTER_PORT
+#export NODE_RANK=$NODE_RANK
+#export NCCL_TREE_THRESHOLD=0
+#export NCCL_ALGO=Ring
+#export NCCL_DEBUG=INFO
 
 exp_name=debug_pretrain
 data_root=/blob/v-jinx/data/VilT_dataset
