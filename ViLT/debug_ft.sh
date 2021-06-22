a3916426dc31a9b244451dc4646ee6c7f204a49c
Jxu-Thu <jxu3425@gmail.com>
2021-06-22 12:57:47 +0800

    d

--- ViLT/debug_ft.sh
+++ ViLT/debug_ft.sh
@@ -1,7 +1,7 @@
 DATA_ROOT=/blob/v-jinx/data/VilT_dataset
 EXP_NAME=pretrain_indomain24GPU_vqa_ft_debug
 LOG_DIR=/blob/v-jinx/checkpoint_vilt/finetune
-RESUME_DIR=${LOG_DIR}/${EXP_NAME}/version_2/checkpoints/last.ckpt
+RESUME_DIR=${LOG_DIR}/pretrain_indomain24GPU/version_2/checkpoints/last.ckpt
 
 python run_pdb.py with data_root=$DATA_ROOT log_dir=$LOG_DIR \
 exp_name=$EXP_NAME num_gpus=1 num_nodes=1 \
