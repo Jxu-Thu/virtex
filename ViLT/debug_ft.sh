9533c34fbb0d832dce7f04042150250aa1970b4c
Jxu-Thu <jxu3425@gmail.com>
2021-06-22 12:53:52 +0800

    d

--- ViLT/debug_ft.sh
+++ ViLT/debug_ft.sh
@@ -1,6 +1,9 @@
 DATA_ROOT=/blob/v-jinx/data/VilT_dataset
+EXP_NAME=pretrain_indomain24GPU_vqa_ft_debug
+LOG_DIR=/blob/v-jinx/checkpoint_vilt/finetune
+RESUME_DIR=${LOG_DIR}/${EXP_NAME}/version_2/checkpoints/last.ckpt
 
-python -m pdb run_pdb.py with data_root=$DATA_ROOT \
-num_gpus=1 num_nodes=1 \
+python run.py with data_root=$DATA_ROOT log_dir=$LOG_DIR \
+exp_name=$EXP_NAME num_gpus=1 num_nodes=1 \
 task_finetune_vqa_randaug \
 per_gpu_batchsize=32 load_path=$RESUME_DIR
--- ViLT/submit/submit_finetune1.sh
+++ ViLT/submit/submit_finetune1.sh
@@ -55,11 +55,12 @@ export NCCL_TREE_THRESHOLD=0
 export NCCL_ALGO=Ring
 export NCCL_DEBUG=INFO
 
+EXP_NAME=pretrain_indomain24GPU_vqa_ft
 DATA_ROOT=/blob/v-jinx/data/VilT_dataset
-LOG_DIR=/blob/v-jinx/checkpoint_vilt/pre_train
+LOG_DIR=/blob/v-jinx/checkpoint_vilt/finetune
 RESUME_DIR=${LOG_DIR}/${EXP_NAME}/version_2/checkpoints/last.ckpt
 
-python run.py with data_root=$DATA_ROOT \
-num_gpus=$N_GPU_PER_WORKER num_nodes=${N_WORKER} \
+python run.py with data_root=$DATA_ROOT log_dir=$LOG_DIR \
+exp_name=$EXP_NAME num_gpus=$N_GPU_PER_WORKER num_nodes=${N_WORKER} \
 task_finetune_vqa_randaug \
 per_gpu_batchsize=64 load_path=$RESUME_DIR
