8e74e2b9f1e2c96fba07fd49a95a73427748a91d
Jxu-Thu <jxu3425@gmail.com>
2021-06-22 12:36:38 +0800

    d

--- ViLT/debug_ft.sh
+++ ViLT/debug_ft.sh
@@ -1,6 +1,6 @@
 DATA_ROOT=/blob/v-jinx/data/VilT_dataset
 
-python run.py with data_root=$DATA_ROOT \
+python -m pdb run.py with data_root=$DATA_ROOT \
 num_gpus=1 num_nodes=1 \
 task_finetune_vqa_trainval_randaug \
 per_gpu_batchsize=32 load_path=$RESUME_DIR
