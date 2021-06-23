c8f7be6e3802f0bb70d93a7cafb5989ea55e4e03
Jxu-Thu <jxu3425@gmail.com>
2021-06-23 12:59:40 +0800

    d

--- ViLT/debug.sh
+++ ViLT/debug.sh
@@ -12,6 +12,6 @@ TOTAL_BATCH_SIZE=4320
 
 python run_pdb.py with data_root=$DATA_ROOT log_dir=$LOG_DIR \
 exp_name=$EXP_NAME num_gpus=$N_GPU_PER_WORKER \
-num_nodes=${N_WORKER} task_mlm_itm_indomain_nopretrain whole_word_masking=True \
+num_nodes=${N_WORKER} task_mlm_itm_indomain whole_word_masking=True \
 resume_from=$RESUME_DIR \
 step100k batch_size=$TOTAL_BATCH_SIZE per_gpu_batchsize=$BATCH_SIZE
