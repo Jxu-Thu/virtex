422c1cc12bf2694067d3865e013730ffa888a87e
Jxu-Thu <jxu3425@gmail.com>
2021-06-23 18:51:28 +0800

    d

--- ViLT/submit/submit_hwrun1.sh
+++ ViLT/submit/submit_hwrun1.sh
@@ -23,7 +23,8 @@ CKPT_DIR=s3://bucket-7001/luoxu/dataset/MMT/vilt_checkpoint/${EXP_NAME}
 python run.py with data_root=$DATA_ROOT log_dir=$LOG_DIR \
 exp_name=$EXP_NAME num_gpus=$N_GPU_PER_WORKER \
 num_nodes=${N_WORKER} task_mlm_itm_indomain whole_word_masking=True \
-huawei_target_dir=$CKPT_DIR \
+huawei_target_dir=$DATA_ROOT \
+huawei_root_path=/cache/VilT_dataset \
 step100k per_gpu_batchsize=$BATCH_SIZE
 
 #python run.py with data_root=$DATA_ROOT log_dir=$LOG_DIR \
