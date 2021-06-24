8e8f3b917991cdb3ebbc63e13f1745f49209164a
Jxu-Thu <jxu3425@gmail.com>
2021-06-24 15:02:20 +0800

    d

--- ViLT/submit/submit_hwrun2.sh
+++ ViLT/submit/submit_hwrun2.sh
@@ -28,7 +28,7 @@ CKPT_DIR=s3://bucket-7001/luoxu/dataset/MMT/vilt_checkpoint/${EXP_NAME}
 
 python run.py with data_root=$DATA_ROOT log_dir=$LOG_DIR \
 exp_name=$EXP_NAME num_gpus=$N_GPU_PER_WORKER \
-num_nodes=${N_WORKER} task_mlm_itm_indomain whole_word_masking=True \
+num_nodes=${N_WORKER} task_mlm_itm_indomain_nopretrain_middle whole_word_masking=True \
 huawei_target_dir=$CKPT_DIR \
 huawei_flag=True \
 huawei_root_path=/cache/VilT_dataset \
