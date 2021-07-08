a51be93a29c7e537a673ad57acc9594110217496
Jxu-Thu <jxu3425@gmail.com>
2021-07-08 21:17:27 +0800

    d

--- ViLT_convstem/submit/submit_run4.sh
+++ ViLT_convstem/submit/submit_run4.sh
@@ -62,7 +62,7 @@ LOG_DIR=/blob/v-jinx/checkpoint_vilt/pre_train
 
 EXP_NAME=pretrain_indomain24GPU_h512_without_pretrain_conv_bottom
 #RESUME_DIR=${LOG_DIR}/${EXP_NAME}/version_0/checkpoints/last.ckpt
-BATCH_SIZE=80
+BATCH_SIZE=64
 TOTAL_BATCH_SIZE=3072
 # accum three
 
@@ -71,7 +71,7 @@ python run.py with data_root=$DATA_ROOT log_dir=$LOG_DIR \
 exp_name=$EXP_NAME num_gpus=$N_GPU_PER_WORKER \
 num_nodes=${N_WORKER} task_mlm_itm_indomain_nopretrain_middle whole_word_masking=True \
 vit=vit_middle_conv_patch32_384 \
-max_steps=120000 \
+max_steps=100000 \
 step100k batch_size=$TOTAL_BATCH_SIZE per_gpu_batchsize=$BATCH_SIZE
 
 #python run.py with data_root=$DATA_ROOT log_dir=$LOG_DIR \
