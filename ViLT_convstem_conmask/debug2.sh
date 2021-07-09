9409ef16594f44181b064ba339a14ce47cf5610d
Jxu-Thu <jxu3425@gmail.com>
2021-07-09 21:11:49 +0800

    d

--- ViLT_convstem_conmask/debug2.sh
+++ ViLT_convstem_conmask/debug2.sh
@@ -3,16 +3,15 @@ LOG_DIR=/blob/v-jinx/checkpoint_vilt/pre_train
 
 
 
-EXP_NAME=pretrain_indomain24GPU_nopretrain_debug
-RESUME_DIR=${LOG_DIR}/${EXP_NAME}/version_0/checkpoints/last.ckpt
-BATCH_SIZE=${BATCH_SIZE:-"32"}
-TOTAL_BATCH_SIZE=64
+EXP_NAME=pretrain_indomain24GPU_h512_without_pretrain_conv_bottom_conmask
+BATCH_SIZE=64
+TOTAL_BATCH_SIZE=3072
 
 
-python run_pdb.py with data_root=$DATA_ROOT log_dir=$LOG_DIR \
-exp_name=$EXP_NAME num_gpus=1 \
-num_nodes=1 task_mlm_itm_indomain_nopretrain_middle whole_word_masking=True \
-resume_from=$RESUME_DIR \
-huawei_root_path=/cache/VilT_dataset \
-huawei_target_dir=/cache/aha \
+python run.py with data_root=$DATA_ROOT log_dir=$LOG_DIR \
+exp_name=$EXP_NAME num_gpus=$N_GPU_PER_WORKER \
+num_nodes=${N_WORKER} task_mlm_itm_indomain_nopretrain_middle whole_word_masking=True \
+vit=vit_middle_conv_patch32_384 \
+max_steps=100000 \
+max_patch_len=14 \
 step100k batch_size=$TOTAL_BATCH_SIZE per_gpu_batchsize=$BATCH_SIZE
