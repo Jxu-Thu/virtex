dda7930a62c22de9dc8dc89f7305386811c90c8a
Jxu-Thu <jxu3425@gmail.com>
2021-07-09 13:27:48 +0800

    d

--- ViLT/submit/submit_run1.sh
+++ ViLT/submit/submit_run1.sh
@@ -61,7 +61,7 @@ LOG_DIR=/blob/v-jinx/checkpoint_vilt/pre_train
 
 #24ka
 EXP_NAME=pretrain_full24GPU
-#RESUME_DIR=${LOG_DIR}/${EXP_NAME}/version_0/checkpoints/last.ckpt
+RESUME_DIR=${LOG_DIR}/${EXP_NAME}/version_2/checkpoints/last.ckpt
 BATCH_SIZE=60
 TOTAL_BATCH_SIZE=4320
 
@@ -69,4 +69,5 @@ TOTAL_BATCH_SIZE=4320
 python run.py with data_root=$DATA_ROOT log_dir=$LOG_DIR \
 exp_name=$EXP_NAME num_gpus=$N_GPU_PER_WORKER \
 num_nodes=${N_WORKER} task_mlm_itm whole_word_masking=True \
+resume_from=$RESUME_DIR \
 step100k batch_size=$TOTAL_BATCH_SIZE per_gpu_batchsize=$BATCH_SIZE
