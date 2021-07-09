19b8e6635e717acc7e8402bd3702d5aabc16d73c
Jxu-Thu <jxu3425@gmail.com>
2021-07-09 14:22:31 +0800

    d

--- ViLT/submit/submit_finetune8.sh
+++ ViLT/submit/submit_finetune8.sh
@@ -63,4 +63,4 @@ RESUME_DIR=/blob/v-jinx/checkpoint_vilt/pre_train/pretrain_full24GPU/version_2/c
 python run.py with data_root=$DATA_ROOT log_dir=$LOG_DIR \
 exp_name=$EXP_NAME num_gpus=$N_GPU_PER_WORKER num_nodes=${N_WORKER} \
 task_finetune_vqa_randaug \
-per_gpu_batchsize= load_path=$RESUME_DIR
+per_gpu_batchsize=32 load_path=$RESUME_DIR
