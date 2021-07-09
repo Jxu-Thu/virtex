42f7e77e64fb4585544f44bdef979addf5ebbcc7
Jxu-Thu <jxu3425@gmail.com>
2021-07-09 13:33:13 +0800

    d

--- ViLT/debug_ft.sh
+++ ViLT/debug_ft.sh
@@ -1,9 +1,9 @@
-EXP_NAME=pretrain_indomain24GPU_vqa__nopretrain_ft_debug
+EXP_NAME=pretrain_vqa_ft_full
 DATA_ROOT=/blob/v-jinx/data/VilT_dataset
-LOG_DIR=/blob/v-jinx/checkpoint_vilt/finetune_val_best
-RESUME_DIR=/blob/v-jinx/checkpoint_vilt/pre_train/pretrain_indomain24GPU_nopretrain/version_2/checkpoints/epoch=79-step=99839.ckpt
+LOG_DIR=/blob/v-jinx/checkpoint_vilt/finetune_full_last
+RESUME_DIR=/blob/v-jinx/checkpoint_vilt/pre_train/pretrain_full24GPU/version_2/checkpoints/last.ckpt
 
-python run_pdb.py with data_root=$DATA_ROOT log_dir=$LOG_DIR \
-exp_name=$EXP_NAME num_gpus=1 num_nodes=1 \
+python run.py with data_root=$DATA_ROOT log_dir=$LOG_DIR \
+exp_name=$EXP_NAME num_gpus=$N_GPU_PER_WORKER num_nodes=${N_WORKER} \
 task_finetune_vqa_randaug \
-per_gpu_batchsize=32 load_path=$RESUME_DIR
+per_gpu_batchsize= load_path=$RESUME_DIR
