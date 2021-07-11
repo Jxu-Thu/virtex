fee7d6b75f42e0f77f8e6a78c15ff60cdedfbae0
Jxu-Thu <jxu3425@gmail.com>
2021-07-11 20:33:16 +0800

    d

--- ViLT_convstem_multirange/run_pdb.py
+++ ViLT_convstem_multirange/run_pdb.py
@@ -74,7 +74,8 @@ def main(_config):
         fast_dev_run=_config["fast_dev_run"],
         val_check_interval=_config["val_check_interval"],
         limit_train_batches=1,
-        limit_val_batches=1
+        limit_val_batches=1,
+        overfit_batches=10
     )
     if not _config["test_only"]:
         trainer.fit(model, datamodule=dm)
