34969a3365185ee5521fcffb8b5dc064e6f1ffdb
Jxu-Thu <jxu3425@gmail.com>
2021-07-11 15:34:56 +0800

    d

---
+++ ViLT_convstem_multirange/debug_try.sh
@@ -0,0 +1,16 @@
+DATA_ROOT=/blob/v-jinx/data/VilT_dataset
+LOG_DIR=/blob/v-jinx/checkpoint_vilt/pre_train
+
+
+
+EXP_NAME=pretrain_indomain24GPU_nopretrain
+RESUME_DIR=${LOG_DIR}/${EXP_NAME}/version_0/checkpoints/last.ckpt
+BATCH_SIZE=32
+TOTAL_BATCH_SIZE=64
+
+
+python run.py with data_root=$DATA_ROOT log_dir=$LOG_DIR \
+exp_name=$EXP_NAME num_gpus=1 \
+num_nodes=1 task_mlm_itm_indomain_nopretrain whole_word_masking=True \
+resume_from=$RESUME_DIR \
+step100k batch_size=$TOTAL_BATCH_SIZE per_gpu_batchsize=$BATCH_SIZE
---
+++ ViLT_convstem_multirange/vilt/utils/write_conceptual_caption.py
@@ -0,0 +1,151 @@
+import json
+import pdb
+
+import pandas as pd
+import pyarrow as pa
+import gc
+import random
+import os
+
+from tqdm import tqdm
+from glob import glob
+
+
+def path2rest(path, iid2captions):
+    split, _, name = path.split("/")[-3:]
+    split = split.split("_")[-1]
+    iid = name
+
+    with open(path, "rb") as fp:
+        binary = fp.read()
+
+    captions = iid2captions[iid]
+
+    return [
+        binary,
+        captions,
+        iid,
+        split,
+    ]
+
+def path2rest_split(path, iid2captions, split):
+    name = path.split("/")[-1]
+    iid = name
+
+    with open(path, "rb") as fp:
+        binary = fp.read()
+
+    captions = iid2captions[iid]
+
+    return [
+        binary,
+        captions,
+        iid,
+        split,
+    ]
+
+
+def make_arrow(root, dataset_root):
+    for split in ["val", "train"]:
+        with open(f"{root}/{split}_annot.json", "r") as fp:
+            captions = json.load(fp)
+
+        iid2captions = dict()
+        for cap in tqdm(captions):
+            iid = cap[0].split("/")[-1]
+            iid2captions[iid] = [cap[1]]
+
+        paths = list(glob(f"{root}/images_{split}/*/*"))
+        random.shuffle(paths)
+        caption_paths = [path for path in paths if path.split("/")[-1] in iid2captions]
+        if len(paths) == len(caption_paths):
+            print("all images have caption annotations")
+        else:
+            print("not all images have caption annotations")
+        print(
+            len(paths), len(caption_paths), len(iid2captions),
+        )
+
+        sub_len = int(len(caption_paths) // 100000)
+        subs = list(range(sub_len + 1))
+        for sub in subs:
+            sub_paths = caption_paths[sub * 100000 : (sub + 1) * 100000]
+            bs = [path2rest(path, iid2captions) for path in tqdm(sub_paths)]
+            dataframe = pd.DataFrame(
+                bs, columns=["image", "caption", "image_id", "split"],
+            )
+
+            table = pa.Table.from_pandas(dataframe)
+
+            os.makedirs(dataset_root, exist_ok=True)
+            with pa.OSFile(
+                f"{dataset_root}/conceptual_caption_{split}_{sub}.arrow", "wb"
+            ) as sink:
+                with pa.RecordBatchFileWriter(sink, table.schema) as writer:
+                    writer.write_table(table)
+            del dataframe
+            del table
+            del bs
+            gc.collect()
+
+def make_arrow_blob(root, dataset_root):
+    for split in ["val", "train"]:
+        if split == 'val':
+            tsv_file = f"{root}/validation_url_caption.tsv"
+            paths = list(glob(f"{root}/validation/*"))
+            file_split = 'images_val'
+        else:
+            tsv_file = f"{root}/training_url_caption.tsv"
+            paths = list(glob(f"{root}/training/*"))
+            file_split = 'images_train'
+        captions = pd.read_csv(tsv_file, sep='\t') # columns = ['img', 'folder', 'type', 'x', 'http_status', 'url', 'caption']
+        # some do not obtain the img
+        captions = captions.dropna(axis=0, subset=['img'])
+        captions['img_file_name'] = captions['img'].apply(lambda x: x.split('/')[-1])
+        # with open(f"{root}/{split}_annot.json", "r") as fp:
+        #     captions = json.load(fp)
+
+        iid2captions = captions[['img_file_name', 'caption']].set_index('img_file_name').squeeze().T.to_dict()
+        # 'training/0_2901536091': 'a very typical bus station',
+
+        # iid2captions = dict()
+        # for cap in tqdm(captions):
+        #     iid = cap[0].split("/")[-1]
+        #     iid2captions[iid] = [cap[1]]
+
+        # paths = list(glob(f"{root}/images_{split}/*/*")) # /blob/v-jinx/data/cc3m/validation/1962_81346558
+        random.shuffle(paths)
+        caption_paths = [path for path in paths if path.split("/")[-1] in iid2captions]
+        if len(paths) == len(caption_paths):
+            print("all images have caption annotations")
+        else:
+            print("not all images have caption annotations")
+        print(
+            len(paths), len(caption_paths), len(iid2captions),
+        )
+
+        sub_len = int(len(caption_paths) // 100000)
+        subs = list(range(sub_len + 1))
+        for sub in subs:
+            sub_paths = caption_paths[sub * 100000 : (sub + 1) * 100000]
+            # for debug
+            bs = [path2rest_split(path, iid2captions, file_split) for path in tqdm(sub_paths)]
+            dataframe = pd.DataFrame(
+                bs, columns=["image", "caption", "image_id", "split"],
+            )
+
+            table = pa.Table.from_pandas(dataframe)
+
+            os.makedirs(dataset_root, exist_ok=True)
+            with pa.OSFile(
+                f"{dataset_root}/conceptual_caption_{split}_{sub}.arrow", "wb"
+            ) as sink:
+                with pa.RecordBatchFileWriter(sink, table.schema) as writer:
+                    writer.write_table(table)
+            del dataframe
+            del table
+            del bs
+            gc.collect()
+# 2910226
+if __name__ == '__main__':
+    make_arrow_blob('/blob/v-jinx/data/cc3m', '/blob/v-jinx/data/VilT_dataset')
