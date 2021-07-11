7dfcfb89017c11071ef81ef5664fb460103281ab
Jxu-Thu <jxu3425@gmail.com>
2021-07-11 11:41:14 +0800

    d

--- ViLT_convstem_conmask/debug2.sh
+++ ViLT_convstem_conmask/debug2.sh
@@ -11,8 +11,8 @@ TOTAL_BATCH_SIZE=3096
 
 
 python run.py with data_root=$DATA_ROOT log_dir=$LOG_DIR \
-exp_name=$EXP_NAME num_gpus=$N_GPU_PER_WORKER \
-num_nodes=${N_WORKER} task_mlm_itm_indomain_nopretrain_middle whole_word_masking=True \
+exp_name=$EXP_NAME num_gpus=1 \
+num_nodes=1 task_mlm_itm_indomain_nopretrain_middle whole_word_masking=True \
 vit=vit_middle_conv_patch32_384_c \
 convc=True \
 max_steps=100000 \
