3949f669707bcfae85582d2ed1df9989a90e39f4
Jxu-Thu <jxu3425@gmail.com>
2021-07-14 11:46:16 +0800

    d

--- ViLT_convstem_conmask/debug_ft.sh
+++ ViLT_convstem_conmask/debug_ft.sh
@@ -4,6 +4,8 @@ LOG_DIR=/blob/v-jinx/checkpoint_vilt/finetune_val_best
 RESUME_DIR=/blob/v-jinx/checkpoint_vilt/pre_train/pretrain_indomain24GPU_h512_without_pretrain_conv_bottom_conmask/version_4/checkpoints/last.ckpt
 SAVE_DIR=$LOG_DIR/$EXP_NAME
 
+mkdir -p SAVE_DIR
+
 python run.py with data_root=$DATA_ROOT log_dir=$LOG_DIR \
 exp_name=$EXP_NAME num_gpus=1 num_nodes=1 \
 vit=vit_middle_conv_patch32_384 \
