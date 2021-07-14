b2eefb009f3fbf3531004fa5f23721a0569755a5
Jxu-Thu <jxu3425@gmail.com>
2021-07-14 11:50:47 +0800

    d

--- ViLT_convstem_conmask/debug_ft.sh
+++ ViLT_convstem_conmask/debug_ft.sh
@@ -9,5 +9,5 @@ mkdir -p SAVE_DIR
 python run.py with data_root=$DATA_ROOT log_dir=$LOG_DIR \
 exp_name=$EXP_NAME num_gpus=1 num_nodes=1 \
 vit=vit_middle_conv_patch32_384 \
-task_finetune_vqa_randaug \
+task_finetune_middle_vqa_randaug \
 per_gpu_batchsize=32 load_path=$RESUME_DIR | tee -a $SAVE_DIR/log.txt
