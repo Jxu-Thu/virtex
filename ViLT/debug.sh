d97b37d7dfacc9fc422ec9741c7c675e941ea613
Jxu-Thu <jxu3425@gmail.com>
2021-07-14 20:38:22 +0800

    d

--- ViLT/debug.sh
+++ ViLT/debug.sh
@@ -6,5 +6,5 @@ RESUME_DIR=/blob/v-jinx/checkpoint_vilt/pre_train/pretrain_full24GPU/version_2/c
 python run_pdb.py with data_root=$DATA_ROOT log_dir=$LOG_DIR \
 exp_name=$EXP_NAME num_gpus=1 num_nodes=1 \
 task_finetune_irtr_f30k_randaug \
-per_gpu_batchsize=32 load_path=$RESUME_DIR
+per_gpu_batchsize=4 load_path=$RESUME_DIR
 
