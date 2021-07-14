9329933b60e3facb209c4e5032b217d4bb555473
Jxu-Thu <jxu3425@gmail.com>
2021-07-14 11:50:17 +0800

    d

--- ViLT_convstem_conmask/submit/submit_finetune10.sh
+++ ViLT_convstem_conmask/submit/submit_finetune10.sh
@@ -67,5 +67,5 @@ mkdir -p SAVE_DIR
 python run.py with data_root=$DATA_ROOT log_dir=$LOG_DIR \
 exp_name=$EXP_NAME num_gpus=$N_GPU_PER_WORKER num_nodes=${N_WORKER} \
 vit=vit_middle_conv_patch32_384 \
-task_finetune_vqa_randaug \
+task_finetune_middle_vqa_randaug \
 per_gpu_batchsize=32 load_path=$RESUME_DIR | tee -a $SAVE_DIR/log.txt
