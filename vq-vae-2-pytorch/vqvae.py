6e10e4d0e9ada088e6522a010fd0b92b4b46ed95
Jxu-Thu <jxu3425@gmail.com>
2021-07-15 16:02:23 +0800

    d

--- vq-vae-2-pytorch/vqvae.py
+++ vq-vae-2-pytorch/vqvae.py
@@ -174,11 +174,11 @@ class VQVAE(nn.Module):
         super().__init__()
 
         self.enc_b = Encoder(in_channel, channel, n_res_block, n_res_channel, stride=4)
-        self.enc_t = Encoder(channel, channel, n_res_block, n_res_channel, stride=2)
+        self.enc_t = Encoder(channel, channel, n_res_block, n_res_channel, stride=4)
         self.quantize_conv_t = nn.Conv2d(channel, embed_dim, 1)
         self.quantize_t = Quantize(embed_dim, n_embed)
         self.dec_t = Decoder(
-            embed_dim, embed_dim, channel, n_res_block, n_res_channel, stride=2
+            embed_dim, embed_dim, channel, n_res_block, n_res_channel, stride=4
         )
         self.quantize_conv_b = nn.Conv2d(embed_dim + channel, embed_dim, 1)
         self.quantize_b = Quantize(embed_dim, n_embed)
