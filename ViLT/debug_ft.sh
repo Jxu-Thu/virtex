2b2dcb6b03c666010f0e39f5e5b21c52a77ef220
Jxu-Thu <jxu3425@gmail.com>
2021-07-20 16:26:13 +0800

    d

--- ViLT/debug_ft.sh
+++ ViLT/debug_ft.sh
@@ -1,9 +1,9 @@
-EXP_NAME=pretrain_nlvr_ft_full_debug
+EXP_NAME=pretrain_flick_ft_full_debug
 DATA_ROOT=/blob/v-jinx/data/VilT_dataset
 LOG_DIR=/blob/v-jinx/checkpoint_vilt/finetune_full_last
 RESUME_DIR=/blob/v-jinx/checkpoint_vilt/pre_train/pretrain_full24GPU/version_2/checkpoints/last.ckpt
 
 python run_pdb.py with data_root=$DATA_ROOT log_dir=$LOG_DIR \
 exp_name=$EXP_NAME num_gpus=1 num_nodes=1 \
-task_finetune_nlvr2_randaug \
-per_gpu_batchsize=16 load_path=$RESUME_DIR
+task_finetune_irtr_f30k_randaug \
+per_gpu_batchsize=4 load_path=$RESUME_DIR
--- ViLT/vilt/modules/vilt_module.py
+++ ViLT/vilt/modules/vilt_module.py
@@ -187,6 +187,8 @@ class ViLTransformerSS(pl.LightningModule):
         return ret
 
     def forward(self, batch):
+        import pdb
+        pdb.set_trace()
         ret = dict()
         if len(self.current_tasks) == 0:
             ret.update(self.infer(batch))
